cb6ae726697f888cdce0373a5a4e6a559a6fd99e
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-09-08 22:38:28 -0300

    Creacion del Proyecto Java

---
+++ src/main/java/com/mycompany/practicogui5/PracticoGUI5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicogui5;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class PracticoGUI5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
